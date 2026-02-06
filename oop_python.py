7e130b1835e451fe22d03f2870961ee6182a3ac1
anujthakreG <anujthakre18@gmail.com>
2026-02-06 23:22:22 +0530

    Add files via upload

--- oop_python.py
+++ oop_python.py
@@ -8,15 +8,19 @@ class person:
 
 a = person() # a is the object of class person
 # a.name = "krishna"
-# a.occupation = "student"     --> by this you can change 
+# a.occupation = "student"     #--> by this you can change 
 # print(a.name,a.age)
 # print(a.name)
-a.info() 
-
+# a.info() 
+# print(person.occupation)
+# print(a.occupation)
 
 """
 Constructoors :- 
 default and parameterized constructors 
+whenerver you create a constructor wither you have to declare the variable or assume variable as no input  
+so while creating object of that particular class you always have to pass the argument value  
+Python does not support multiple contructors(__init__) -> so it is thereby replaced by @classmethod
 """
 
 print("\n\t constructor ! \t\n ")
@@ -44,7 +48,7 @@ made with @ --> for example @demo_decorator
 Inheritance :- 
 """
 
-class Employee :
+class Employee :  # -> parent class 
     def __init__(self,name,id):
         self.name = name
         self.id = id
@@ -52,7 +56,7 @@ class Employee :
     def showDetails(self):
         print(f"the name of employee {self.id} is {self.name}")
 
-class Programmer(Employee):
+class Programmer(Employee):  # -> child class
     def showLangguage(self):
         print("the default language is python ")
 
@@ -90,7 +94,7 @@ class f :
 a2 = f()
 # print(a2.__name)  -> it will throw error 
 
-print(a2._f__name)  # method to acces the private variable 
+print(a2._f__name)  # method to acces the private variable   {obj._classname.__varname}
 
 
 
@@ -165,12 +169,182 @@ Agar data sabka same ho
 """
 
 
-class Grocery:
+# class Grocery:
 
-    shop_name = "My Grocery"   # ← class variable
+#     shop_name = "My Grocery"   # ← class variable
 
-    def __init__(self, items):
-        self.items = items     # ← instance variable , gets differed with objects/instances 
+#     def __init__(self, items):
+#         self.items = items     # ← instance variable , gets differed with objects/instances 
 
-g1 = Grocery()
-g1.items
+# g1 = Grocery()
+# g1.items
+
+print("\n\t Next subtopic \n\t ")
+
+
+"""
+class method , deecorator name -> @classmethod -> works on the class variables not on the objects 
+"""
+
+# exmaple :
+class Emp :
+    
+    company = "Apple"
+    def show(self):
+        print(f"{self.name} works in the company {self.company}")
+
+    @classmethod
+    def changecompany(cls, Newname):  # you can write anything in the place of cls 
+        cls.company = Newname
+
+co = Emp()
+co.name = "Anuj"
+co.show()
+co.changecompany("tesla")
+co.show()
+print(Emp.company) # class Variable is called like this 
+# co.namee = "hehe"
+print(co.company)   # this both will show the "Apple" in the output !
+
+print("\n\t Continuing Class-Method \n\t ")
+
+"""
+Class Methods as alternative contructors 
+constructors are special type of method which automatically gets executed at the time of object creation 
+there are times when you want to create object in a different manner so for that this method is usefull instead using the default contrucotr for it 
+"""
+
+# numbers = [int(x) for x in input("Enter numbers: ").split()]
+# print(numbers)
+ 
+class Employee2:
+    def __init__(self,name,sal):
+        self.name = name
+        self.sal = sal
+
+
+    @classmethod
+    def fromstr(cls,String):
+        return cls(String.split("-")[0] ,String.split("-")[1])
+    
+e22 = Employee2("john","120")
+print(e22.name)
+print(e22.sal)
+
+String = "johra-1200"
+
+# ee22.fromstr(String)  # it will throw error as the arguments are not passed while creating the object ee22
+ee22 = Employee2.fromstr(String)
+
+# string2 = input().split()   -> not working 
+# ee3 = Employee2.fromstr(string2)
+
+
+print("\n\t Next subtopic \n\t ")
+
+"""
+dir,__dict__,help methods in python 
+"""
+
+# x = [1,2,3]
+# print(dir(x)) # -> "what things are available"
+# # print(__dict__(e22))  # -> "what data is stored ?" 
+# print(help(x))  # -> help you with the "how does it work"
+
+
+print("\n\t Next subtopic \n\t ")
+
+"""
+super keyword -> used to refer the parent class 
+
+"""
+
+class Parentclass :
+    def Parentmethod(self):
+        print("this is Parent class method")
+
+class Childclass(Parentclass):
+    def Childmethod(self):
+        print("this is child method ")
+
+        super().Parentmethod()
+
+c = Parentclass()
+c.Parentmethod()
+c1 = Childclass()
+c1.Childmethod()
+
+"""
+Dunder Method :- 
+| Dunder Method              | Triggered by                      | Purpose                                               |
+| -------------------------- | --------------------------------- | ----------------------------------------------------- |
+| `__init__(self, ...)`      | Object creation → `obj = Class()` | Initializes the object (constructor)                  |
+| `__str__(self)`            | `print(obj)`                      | Returns user-friendly string representation of object |
+| `__len__(self)`            | `len(obj)`                        | Returns length of the object                          |
+| `__add__(self, other)`     | `obj1 + obj2`                     | Defines addition behaviour for objects                |
+| `__eq__(self, other)`      | `obj1 == obj2`                    | Defines equality comparison                           |
+| `__getitem__(self, index)` | `obj[index]`                      | Enables indexing access                               |
+
+"""
+"""
+Method overridding -> method name , parameters must be same and it must be inherited 
+"""
+
+class Animal:
+    def sound(self):
+        print("Animal makes sound")
+
+class Dog(Animal):
+    def sound(self):
+        print("Dog barks")
+
+A = Dog()
+B = Animal()
+A.sound()
+# B.sound()
+
+"""
+dunder method start with __ 
+Operator overloading --> to change the behaviour of the operators 
+"""
+
+"""
+Single Inheritance :- where a class inherits the property from a Single parent class ,default one
+Multiple Inheritance :- child class made by more than 1 parent class 
+Multilevel Inheritance :- Parentclass --> childclass1 --> childclass2(child of childclass1)
+"""
+
+# example for multiple inheritance ->
+
+class Employee:
+    def __init__(self,name):
+        self.name = name
+
+        print(f"the name is {self.name}")
+
+    def show(self):
+        print(f"the name is {self.name}")
+
+class Dancer:
+
+    def __init__(self,dance):
+        self.dance = dance 
+
+    def show(self):
+        print(f"the dance is {self.dance}")
+                                # -> method overriding is here 
+class EmployeeDancer(Employee, Dancer) : # -> if you write Employee parentclass first then that class method will run first
+# class EmployeeDancer(Dancer,Employee) :
+    def __init__(self,name,dance):
+        self.name = name
+        self.dance = dance
+
+o = EmployeeDancer("hariss-ALI-Khan","HipHop")
+print(o.name)
+print(o.dance)
+
+o.show()
+
+
+
+
